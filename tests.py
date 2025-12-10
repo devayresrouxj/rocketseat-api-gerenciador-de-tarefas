69f1bd11daf172401c26c871b8f9fc303be4f2df
Ayres Roux <devayresrouxj@gmail.com>
2025-12-09 21:22:53 -0300

    test: update and delete tasks

--- tests.py
+++ tests.py
@@ -35,3 +35,36 @@ def test_get_task():
 
     response_json = response.json()
     assert task_id == response_json["id"]
+
+def test_update_task():
+  if tasks:
+    task_id = tasks[0]
+    payload = {
+      "title": "Título Atualizado",
+      "description": "Nova descrição",
+      "completed": False
+    }
+    response = requests.put(f"{BASE_URL}/tasks/{task_id}", json=payload)
+    response.status_code == 200
+    
+    response_json = response.json()
+    assert "message" in response_json
+
+    # Nova requisição em tarefa específica
+    response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+    assert response.status_code == 200
+
+    response_json = response.json()
+    assert response_json["title"] == payload["title"]
+    assert response_json["description"] == payload["description"]
+    assert response_json["completed"] == payload["completed"]
+
+def test_delete_task():
+  if tasks:
+    task_id = tasks[0]
+    response = requests.delete(f"{BASE_URL}/tasks/{task_id}")
+    assert response.status_code == 200
+
+    response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+    assert response.status_code == 404
+
